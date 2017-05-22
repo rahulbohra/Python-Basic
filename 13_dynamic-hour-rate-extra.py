e14ff24f29a6fffbfc0a299a201c05c318fbb579
Rahul Bohra <rahulthespaceman@gmail.com>
2017-05-22 17:00:30 +0530

    updated the code logic

--- 13_dynamic-hour-rate-extra.py
+++ 13_dynamic-hour-rate-extra.py
@@ -1,16 +1,16 @@
-
 try :
     hoursInput = raw_input("Enter Hours : ")
     hours = float(hoursInput)
     rateInput = raw_input("Enter Rate : ")
     rate = float(rateInput)
+
+    if hours <= 40 :
+        print hours * rate
+    else :
+        hoursDifference = hours - 40
+        baseValue = 40 * rate
+        extraValue = hoursDifference * (1.5 * rate)
+        print baseValue + extraValue
+
 except :
     print "Please enter numberic values only"
-
-if hours <= 40 :
-    print hours * rate
-else :
-    hoursDifference = hours - 40
-    baseValue = 40 * rate
-    extraValue = hoursDifference * (1.5 * rate)
-    print baseValue + extraValue
