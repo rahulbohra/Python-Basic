65f74f81e6c519d872323fb5430300d9a05fdeeb
Rahul Bohra <rahulthespaceman@gmail.com>
2017-05-22 16:56:02 +0530

    updated the code logic

--- 12_hour-rate-extra.py
+++ 12_hour-rate-extra.py
@@ -5,12 +5,13 @@ extraHourRate = 15
 extraHours = raw_input('How many extra hours you worked had after your basic fixedHours? ')
 try:
     extraHours = int(extraHours)
-    print extraHours
-    if extraHours > 40 :
+    if extraHours > 0 :
         basicPay = basicRate * fixedHours
         extraPay = extraHourRate * extraHours
         totalPay = basicPay + extraPay
         #totalHours = fixedHours + extraHours # Need to find how to include variables in the print statements, hence commented.
         print 'Your total payment is', totalPay
+    else:
+        print 'Number of Hours should be greater then 0'
 except:
     print 'Instead of typing number you entered -', extraHours
