9c6e91ef98df8adfbef3134f4d16a2e3f21a1ae4
Rahul Bohra <rahulthespaceman@gmail.com>
2017-05-29 14:17:22 +0530

    Basic Function in Python

---
+++ 18_basic_function.py
@@ -0,0 +1,11 @@
+x = 5
+print 'Hello'
+
+def print_lyrics():
+    print "I'm a lumberjack, and I 'm okay.'"
+    print 'I sleep all the night and I work all the day.'
+
+print 'Yo'
+x = x + 2
+print x
+print_lyrics()
