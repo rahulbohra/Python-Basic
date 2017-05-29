c66d016234f835044883279527804c98774a1073
Rahul Bohra <rahulthespaceman@gmail.com>
2017-05-29 20:39:37 +0530

    While Loop with CONTINUE statement

---
+++ 25_while_loop_with_continue.py
@@ -0,0 +1,9 @@
+while True:
+    input = raw_input('>')
+    # skip the current iteration and jump to the top of the loop and starts the next iteration. Hence, line 8 was printed.
+    if input[0] == '#':
+        continue
+    if input == 'done':
+        break
+    print 'You entered -',input
+print 'Out of while Loop.'
