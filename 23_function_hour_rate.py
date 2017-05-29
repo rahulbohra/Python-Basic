730baf74bc0ecb2c969513d072d1f0e2a31d2c3a
Rahul Bohra <rahulthespaceman@gmail.com>
2017-05-29 15:36:27 +0530

    Hour Rate Calculator using Function

---
+++ 23_function_hour_rate.py
@@ -0,0 +1,23 @@
+def computePay():
+    try :
+        hoursInput = raw_input("Enter Hours : ")
+        hours = float(hoursInput)
+        rateInput = raw_input("Enter Rate : ")
+        rate = float(rateInput)
+
+        if hours <= 40 :
+            print hours * rate
+        else :
+            hoursDifference = hours - 40
+            baseValue = 40 * rate
+            extraValue = hoursDifference * (1.5 * rate)
+            print baseValue + extraValue
+
+    except :
+        print "Please enter numberic values only"
+
+print "-------------------------------------------------------------------------"
+print "                       Hour Rate Calculator                              "
+print "-------------------------------------------------------------------------"
+
+computePay()
