8f491b122eae2cd55a4ef2cc12e5b296a9a19ec9
Rahul Bohra <rahulthespaceman@gmail.com>
2017-05-30 14:19:34 +0530

    Filtering the array numbers using FOR loop

---
+++ 32_filtering_the_number.py
@@ -0,0 +1,7 @@
+data = [12,4,21,56,01,42,76,93]
+print data
+print '\nScript starts\n'
+for number in data:
+    if number > 50:
+        print 'Number greater then 50 is ', number
+print '\nScript Ends'
