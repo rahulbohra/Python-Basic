517ca4b36b26bfc69bfd85225dc69a1e1b14f980
Rahul Bohra <rahulthespaceman@gmail.com>
2017-05-30 13:22:02 +0530

    Summing all the numbers of the array using FOR loop

---
+++ 30_summing_the_numbers.py
@@ -0,0 +1,6 @@
+sum = 0
+print 'Sum of all the numbers BEFORE processing : ', sum
+for numbers in [1,5,96,3,7,23,125,56,]:
+    sum = sum + numbers
+    print sum, numbers
+print 'Sum of all the numbers AFTER processing : ', sum
