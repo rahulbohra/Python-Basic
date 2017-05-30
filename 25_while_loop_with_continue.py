6f1d219a19df42caafbcff76daf1b3afef474226
Rahul Bohra <rahulthespaceman@gmail.com>
2017-05-30 11:59:33 +0530

    comment updated

--- 25_while_loop_with_continue.py
+++ 25_while_loop_with_continue.py
@@ -1,6 +1,6 @@
 while True:
     input = raw_input('>')
-    # skip the current iteration and jump to the top of the loop and starts the next iteration. Hence, line 8 was printed.
+    # skip the current iteration and jump to the top of the loop and starts the next iteration. Hence, line 8 was not printed.
     if input[0] == '#':
         continue
     if input == 'done':
