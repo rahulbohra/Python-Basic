7048c6508e16964ccf83ecba52c2d7cc0b6597ec
Rahul Bohra <rahulthespaceman@gmail.com>
2017-05-30 15:22:48 +0530

    Update the comment

--- 34_smallest_value.py
+++ 34_smallest_value.py
@@ -1,6 +1,7 @@
 smallest_value = None
 print 'BEFORE processing : ',smallest_value
 for value in [9,2,4,6,8,5,9,3,7,1,0.3,2,0.01,0.5]:
+    # Here, "is" is the replica of "=="
     if smallest_value is None:
         smallest_value = value
     elif value < smallest_value:
