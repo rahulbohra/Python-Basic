117d9459d58c9af00d5c6d6b715bde7a3523c922
Rahul Bohra <rahulthespaceman@gmail.com>
2017-05-30 15:18:25 +0530

    Smallest value using None

---
+++ 34_smallest_value.py
@@ -0,0 +1,9 @@
+smallest_value = None
+print 'BEFORE processing : ',smallest_value
+for value in [9,2,4,6,8,5,9,3,7,1,0.3,2,0.01,0.5]:
+    if smallest_value is None:
+        smallest_value = value
+    elif value < smallest_value:
+        smallest_value = value
+    print smallest_value, value
+print 'AFTER processing : ',smallest_value
