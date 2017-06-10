09e550448d0b7a69b2783b5004c65fdadb7c41d6
Rahul Bohra <rahulthespaceman@gmail.com>
2017-06-10 15:22:19 +0530

    find function of string

---
+++ 43_find_in_string.py
@@ -0,0 +1,8 @@
+# Find the position of the string to be find.
+word = 'Hello Python'
+position = word.find('yt')
+print 'Position :', position
+
+# if the required string not found find() returns -1
+position = word.find('z')
+print 'Position :', position
