119705a31dc076c2b97e4a460053ae2e31f6c4f5
Rahul Bohra <rahulthespaceman@gmail.com>
2017-06-10 14:59:22 +0530

    Using in as a operator

---
+++ 40_in_keyword.py
@@ -0,0 +1,13 @@
+fruit = 'banana'
+count = 0
+if 'n' in fruit:
+    print "We found 'N'"
+    count += 1
+else:
+    print "We didn't found 'N'"
+
+if 'm' in fruit:
+    print "We found 'M'"
+    count += 1
+else:
+    print "We didn't found 'M'"
