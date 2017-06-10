e90445da4bb4b8195fd447bcb33c4a9f56dca41a
Rahul Bohra <rahulthespaceman@gmail.com>
2017-06-10 14:33:33 +0530

    Playing with string using len() function

---
+++ 38_looking_inside_strings.py
@@ -0,0 +1,27 @@
+fruit = 'banana'
+print 'Fruit name :', fruit
+
+fruitLength = len(fruit)
+print 'Fruit Lenght :', fruitLength
+
+print 'Fruit 1st Index letter :', fruit[1]
+
+n = 3
+print 'Fruit nth - 1 Index letter :', fruit[n - 1]
+
+print "\n----------------- Print string with index numbers using WHILE loop --------------------\n"
+index = 0
+while index < fruitLength:
+    print index, fruit[index]
+    index += 1
+
+print "\n----------------- Print string using FOR loop --------------------\n"
+for letter in fruit:
+    print letter
+
+print "\n----------------- Counting letter 'a' in string using FOR loop --------------------\n"
+count = 0
+for letter in fruit:
+    if letter == 'a':
+        count += 1
+print "Number of A's in fruit is", count
