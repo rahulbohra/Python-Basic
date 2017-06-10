98a2e19511b0c85c857798ca968ef1451fe3a585
Rahul Bohra <rahulthespaceman@gmail.com>
2017-06-10 14:52:38 +0530

    Some other slice examples

--- 39_string_slice.py
+++ 39_string_slice.py
@@ -8,8 +8,19 @@ musketeer_2 = musketeersName[4:8]
 print 'Second musketeer :', musketeer_2
 
 musketeer_3 = musketeersName[9:14]
-print 'Third musketeer :', musketeer_3
 
-print "\n----------------- Special case, in case if you are not sure about the final length of the string --------------------\n"
+print 'Third musketeer :', musketeer_3
+print "\n----------------- Special case STARTS, in case if you are not sure about the final length of the string --------------------\n"
 musketeer_3 = musketeersName[9:20]
 print 'Third musketeer [9:20], it should be [9:14]:', musketeer_3
+
+print "\n----------------- Special case ENDS, in case if you are not sure about the final length of the string --------------------\n"
+
+firstTwoChars = musketeersName[:2]
+print 'First Two characters of the string :', firstTwoChars
+
+lastTwoChars = musketeersName[12:]
+print 'First Two characters of the string :', lastTwoChars
+
+allChars = musketeersName[:]
+print 'All characters of the string :', allChars
