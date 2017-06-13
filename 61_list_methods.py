5afda136e0a454656afdaf2a7b0dc8b961864768
Rahul Bohra <rahulthespaceman@gmail.com>
2017-06-13 16:28:28 +0530

    lists methods, using in also

---
+++ 61_list_methods.py
@@ -0,0 +1,9 @@
+people = list()
+people.append("Kiwi")
+people.append(["Nanu", "Nani"])
+people.append("Mama")
+print people
+
+# Using "in", return boolean
+print "Kiwi" in people # True
+print "Kiwi Dakri" in people # False
