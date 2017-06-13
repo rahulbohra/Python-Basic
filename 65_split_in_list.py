c9be60a3e023b2f41c15e5cd4bd3f9d0fab6e016
Rahul Bohra <rahulthespaceman@gmail.com>
2017-06-13 20:17:33 +0530

    Split in lists

---
+++ 65_split_in_list.py
@@ -0,0 +1,14 @@
+people = "Happy Birthday Nani"
+names = people.split()
+
+print names
+print len(names)
+
+for name in names:
+    print name
+
+print "\n"
+fact = "Python:PHP:C"
+languages = fact.split(":")
+for language in languages:
+    print language
