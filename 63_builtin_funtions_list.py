ddeeebb4f65cd12c20e3c1bcd94e4252952bf11a
Rahul Bohra <rahulthespaceman@gmail.com>
2017-06-13 16:50:21 +0530

    Applying Built-in functions for list

---
+++ 63_builtin_funtions_list.py
@@ -0,0 +1,6 @@
+data = [1,2,3,4,5,6,7,8,9,10]
+print "Length of numbers :", len(data)
+print "Maximum number :", max(data)
+print "Minimum number :", min(data)
+print "Summation of numbers :", sum(data)
+print "Average of numbers :", sum(data) / len(data)
