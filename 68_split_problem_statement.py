f74d4abe3115731c1dbdf0faf706e4a71fa2065b
Rahul Bohra <rahulthespaceman@gmail.com>
2017-06-14 13:28:32 +0530

    Amazing Problem Statement

---
+++ 68_split_problem_statement.py
@@ -0,0 +1,50 @@
+'''
+Open the file romeo.txt and read it line by line.
+For each line, split the line into a list of words using the split() method.
+The program should build a list of words.
+For each word on each line check to see if the word is already in the list and if not append it to the list.
+When the program completes, sort and print the resulting words in alphabetical order.
+'''
+'''
+fileName = raw_input("Enter the filename : ")
+openFile = open(fileName)
+lineList = newList = list()
+for line in openFile:
+    line = line.rstrip()
+    lineList = line.split()
+    newList += lineList
+newList.sort()
+print newList
+'''
+fileName = raw_input("Enter the filename : ")
+openFile = open(fileName)
+lineList = mergeList = newList = list()
+for line in openFile:
+    line = line.rstrip()
+    lineList = line.split()
+    for word in lineList:
+        if not word in newList:
+            newList.append(word)
+        #if not lineList in newList:
+        #    newList.append(lineList)
+
+
+
+
+'''
+mergeList += lineList
+
+for word in lineList:
+    if not lineList in newList:
+        newList.append(lineList)
+'''
+newList.sort()
+print newList
+'''
+    for word in lineList:
+        if not lineList in newList:
+    	       newList.append(lineList)
+            #newList += lineList
+newList.sort()
+print newList
+'''
