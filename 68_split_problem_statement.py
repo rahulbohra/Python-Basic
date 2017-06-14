c151c5ac38574d12974cfd6b8baa9e0dab846481
Rahul Bohra <rahulthespaceman@gmail.com>
2017-06-14 13:32:37 +0530

    changes

--- 68_split_problem_statement.py
+++ 68_split_problem_statement.py
@@ -5,17 +5,6 @@ The program should build a list of words.
 For each word on each line check to see if the word is already in the list and if not append it to the list.
 When the program completes, sort and print the resulting words in alphabetical order.
 '''
-'''
-fileName = raw_input("Enter the filename : ")
-openFile = open(fileName)
-lineList = newList = list()
-for line in openFile:
-    line = line.rstrip()
-    lineList = line.split()
-    newList += lineList
-newList.sort()
-print newList
-'''
 fileName = raw_input("Enter the filename : ")
 openFile = open(fileName)
 lineList = mergeList = newList = list()
@@ -25,26 +14,5 @@ for line in openFile:
     for word in lineList:
         if not word in newList:
             newList.append(word)
-        #if not lineList in newList:
-        #    newList.append(lineList)
-
-
-
-
-'''
-mergeList += lineList
-
-for word in lineList:
-    if not lineList in newList:
-        newList.append(lineList)
-'''
 newList.sort()
 print newList
-'''
-    for word in lineList:
-        if not lineList in newList:
-    	       newList.append(lineList)
-            #newList += lineList
-newList.sort()
-print newList
-'''
