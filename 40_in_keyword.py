2443533551baa23c6fb8d5b2acdf58d6af19d3db
Rahul Bohra <rahulthespaceman@gmail.com>
2017-07-17 20:56:09 +0530

    Removed unnecessary varibale

--- 40_in_keyword.py
+++ 40_in_keyword.py
@@ -2,12 +2,10 @@ fruit = 'banana'
 count = 0
 if 'n' in fruit:
     print "We found 'N'"
-    count += 1
 else:
     print "We didn't found 'N'"
 
 if 'm' in fruit:
     print "We found 'M'"
-    count += 1
 else:
     print "We didn't found 'M'"
