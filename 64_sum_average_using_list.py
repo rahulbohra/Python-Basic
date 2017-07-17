6f48f458937f648e8bf00660781c536a6db1bd93
Rahul Bohra <rahulthespaceman@gmail.com>
2017-07-17 21:43:27 +0530

    Updated the logic

--- 64_sum_average_using_list.py
+++ 64_sum_average_using_list.py
@@ -1,10 +1,13 @@
 numberList = list()
 while True:
     input = raw_input("Enter a number : ")
-    if input == "done":
-        break
-    number = float(input)
-    numberList.append(number)
+    if input == "done": break
+
+    try :
+        number = float(input)
+        numberList.append(number)
+    except :
+        print 'Please enter only numbers. Instead of number you have entered -', input
 
 average = sum(numberList) / len(numberList)
 print "Average :", average
