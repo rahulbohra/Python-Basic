bef6e3b13c2e524f606d6ef4157df93933548c22
Rahul Bohra <rahulthespaceman@gmail.com>
2017-07-17 20:19:36 +0530

    Convert variable from int to float, in order to accept floating value as well

--- 11_first-try-except.py
+++ 11_first-try-except.py
@@ -1,6 +1,7 @@
 number = raw_input('Enter a number to find a square : ')
 try :
-  actualNumber = int(number)**2
-  print 'Square of the number is', actualNumber
+    # In order to accept floating numbers, we are converting the varibale to float.
+    actualNumber = float(number)**2
+    print 'Square of the number is', actualNumber
 except :
   print 'Instead of typing number you entered -', number
