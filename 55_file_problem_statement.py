057693644b462ab577d2ed459c9562a480fb075f
Rahul Bohra <rahulthespaceman@gmail.com>
2017-07-17 21:18:49 +0530

    Extra space added

--- 55_file_problem_statement.py
+++ 55_file_problem_statement.py
@@ -2,7 +2,7 @@
 Write a program that prompts for a file name, then opens that file and reads through the file,
 and print the contents of the file in upper case.
 '''
-fileName = raw_input('Enter file name :')
+fileName = raw_input('Enter file name : ')
 openFile = open(fileName)
 readFile = openFile.read().upper().strip()
 print readFile
