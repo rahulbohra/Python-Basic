13c1ea44fb8dc78a7e2ee148553782824d5185c4
Rahul Bohra <rahulthespaceman@gmail.com>
2017-07-17 20:59:36 +0530

    Removed unnecessary varibles

--- 45_strip_of_string.py
+++ 45_strip_of_string.py
@@ -1,20 +1,18 @@
 # Strip of string is basically trimming the white spaces.
 print '---------------------- STRIP ----------------------\n'
-word = '    qwerty'
+word = '    qwerty    '
 print 'Before :', word
 
 stripWord = word.strip()
 print 'After :', stripWord
 
 print '---------------------- Left STRIP ----------------------\n'
-word = '    qwerty'
 print 'Before :', word
 
 leftStripWord = word.lstrip()
 print 'After :', leftStripWord
 
 print '---------------------- Right STRIP ----------------------\n'
-word = '    qwerty    '
 print 'Before :', word
 
 rightStripWord = word.rstrip()
